956fe1d8a9b3ce0ee10914d0ad439ad407f05f95
st3phhays <steph@stephhays.dev>
2024-02-08 15:03:36 -0600

    Update Astro and Shiki
    Updates the Astro framework to the latest version. New patches for the Synthax theme were generated
    since Shiki was also updated with Astro. This has a temp link to a set version of Playwright, which will
    need to be updated at a later date.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 
+// https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
       theme: 'dracula',
       langs: [],
-      wrap: false,
-    },
+      wrap: false
+    }
   },
   site: "https://bootstrappretty.dev",
   integrations: [sitemap()]
