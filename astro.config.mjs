3506c44e636c994c4a1b02cc69f005bb74a8d45b
st3phhays <steph@stephhays.dev>
2024-10-25 17:36:37 -0500

    WIP Upgrade Everything

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,30 @@
 import { defineConfig } from 'astro/config';
+import icon from "astro-icon";
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
     shikiConfig: {
-      theme: 'dracula',
+      themes: {
+        dracula: 'dracula',
+        'material-theme-palenight': 'material-theme-palenight'
+      },
+      defaultColor: 'dracula',
       langs: [],
       wrap: false
     }
   },
+  vite: {
+    build: {
+      rollupOptions: {
+        external: [
+          "@popperjs/core",
+          "clipboard"
+        ],
+      },
+    },
+  },
   site: "https://bootstrappretty.dev",
-  integrations: [sitemap()]
+  integrations: [icon(), sitemap()]
 });
